ce59c05324923d9407b8da7f7c5eda3389be313f
malinisnr <78588836+malinisnr@users.noreply.github.com>
2021-02-05 00:59:00 -0800

    package version
    package version

--- src/main/java/com/weather/noaaservice/App.java
+++ src/main/java/com/weather/noaaservice/App.java
@@ -9,10 +9,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	private static RESTClient client = new RESTClient();
